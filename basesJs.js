704ffa4fa7799b0f04f71feeee2b5fd5a37a0478
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-09-26 19:14:43 -0600

    Variables

--- basesJs.js
+++ basesJs.js
@@ -5,3 +5,17 @@
 40, 15, 456, 72 //numero
 {"cartucho", "pelotas"}["vaso", "garrafon"] //objeto, array
 true, false //booleano
+
+//Variables
+//Son espacios en memoria donde podemos guardar informacion.
+
+var nombre;//Declaracion de una variable "nombre"
+nombre = "Christopher";//Inicializacion de una variable es darle un valor, en este ejemplo un string "Christopher"
+var nombre = "Christopher";//Declaracion e inicializacion en una sola linea de codigo.
+
+var elementos = ['Computadora', 'Celular'];
+
+var persona = {
+    nombre: 'Christopher',
+    edad: 28
+};
