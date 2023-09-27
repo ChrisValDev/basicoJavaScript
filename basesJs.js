42ce4b0fdf8c8d6d1b630f0be9c0b6cf5bb1ae53
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-09-26 19:08:12 -0600

    Tipos de datos

---
+++ basesJs.js
@@ -0,0 +1,7 @@
+//Tipos de datos
+//Valores 
+
+"Christopher", "Las pelota brinca" //"string"
+40, 15, 456, 72 //numero
+{"cartucho", "pelotas"}["vaso", "garrafon"] //objeto, array
+true, false //booleano
