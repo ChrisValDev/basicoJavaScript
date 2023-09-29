feba4f05072c370bdc32aad2ebe8fc1e9248c4fd
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-09-29 16:30:35 -0600

    Interaccion de texto con HTML desde JS

--- manipulacionDOM/script.js
+++ manipulacionDOM/script.js
@@ -16,3 +16,12 @@ console.log({
     pid,
     input
 }); // usamos console para ver a detalle los valores de cada elemento HTML.
+
+//Interaccion de texto con HTML desde JS
+
+// innerText - propiedad que permite acceder al texto contenido dentro de un elemento HTML, excluyendo las etiquetas HTML. Es útil cuando solo necesitas el texto visible y no el HTML.
+h1.innerText = "Nuevo titulo <s> modificado";
+
+// innerHTML - propiedad permite acceder al contenido HTML dentro de un elemento HTML y también permite modificar dicho contenido. Puedes obtener el contenido de un elemento o asignarle nuevo contenido utilizando innerHTML.
+h1.innerHTML = "Nuevo titulo <s> modificado";
+
