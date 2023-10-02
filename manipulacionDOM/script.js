7283f848317a5d2e9404b0dee4de1e064ce3053e
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-02 11:17:13 -0600

    Otra solucion al mismo escenario para prevenir comportamientos predeterminados en elementos HTML sin usar event.preventDefault

--- manipulacionDOM/script.js
+++ manipulacionDOM/script.js
@@ -7,18 +7,9 @@ const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
-form.addEventListener('submit', sumarInputsValues);
+btn.addEventListener('click', btnOnClick);
 
-function sumarInputsValues(event) {
-    event.preventDefault();//Evita que el formulario se envie.
+function btnOnClick() {
     const sumaInputs = input1.value + input2.value;
     pResult.innerText = "Resultado: " + sumaInputs;
 };
-
-//La propiedad event.preventDefault() se utiliza para prevenir el comportamiento predeterminado de un evento en un elemento HTML. Esto es útil cuando deseas controlar el comportamiento de ciertos eventos, como hacer clic en un enlace (<a>), enviar un formulario o presionar una tecla, sin que la acción predeterminada se ejecute.
-
-/*Estructura
-elemento.addEventListener("evento", function(event) {
-    event.preventDefault();
-    // Tu código personalizado aquí
-});*/
