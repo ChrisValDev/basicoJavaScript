adb1ee7006d4ecf58c39afe7c4e948b4af47e3a4
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-02 11:09:43 -0600

    event.preventDefault - Prevenir comportamiento predeterminado de un elemento HTML con JS

--- manipulacionDOM/script.js
+++ manipulacionDOM/script.js
@@ -1,17 +1,24 @@
 
 
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#formulario');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
-//Estructura de addEventListener (Agrega un escuchador de eventos) es un método en JavaScript que se utiliza para agregar controladores de eventos a elementos HTML de manera más flexible y modular que la asignación de atributos (onclick, etc).
-//elemento.addEventListener(evento, función [opciones, ]);
+form.addEventListener('submit', sumarInputsValues);
 
-btn.addEventListener('click', btnOnClick);
-
-function btnOnClick() {
+function sumarInputsValues(event) {
+    event.preventDefault();//Evita que el formulario se envie.
     const sumaInputs = input1.value + input2.value;
     pResult.innerText = "Resultado: " + sumaInputs;
 };
+
+//La propiedad event.preventDefault() se utiliza para prevenir el comportamiento predeterminado de un evento en un elemento HTML. Esto es útil cuando deseas controlar el comportamiento de ciertos eventos, como hacer clic en un enlace (<a>), enviar un formulario o presionar una tecla, sin que la acción predeterminada se ejecute.
+
+/*Estructura
+elemento.addEventListener("evento", function(event) {
+    event.preventDefault();
+    // Tu código personalizado aquí
+});*/
