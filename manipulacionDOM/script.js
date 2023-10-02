aa08ebdda87265343aed475a13ea4bda5bab7f6f
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-02 10:01:48 -0600

    Selectores para su uso con JS

--- manipulacionDOM/script.js
+++ manipulacionDOM/script.js
@@ -1,3 +1,5 @@
+//El término "selectores" generalmente se utiliza en el contexto de CSS y la manipulación del DOM (Document Object Model) para hacer referencia a la forma en que se eligen elementos HTML específicos para aplicar estilos o realizar manipulaciones con JavaScript.
+//En JavaScript, cuando trabajas con el DOM y deseas interactuar con elementos HTML, puedes utilizar métodos como document.querySelector o document.querySelectorAll para seleccionar elementos específicos en función de selectores CSS y luego realizar acciones en esos elementos.
 
 const h1 = document.querySelector('h1');
 const input1 = document.querySelector('#calculo1');
