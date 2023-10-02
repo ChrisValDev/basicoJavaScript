e45e7334d11106109b2635cf4176727c5837dba3
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-02 10:40:30 -0600

    addEventListener - Mostrando resultado en la misma pagina del navegador con JS

--- manipulacionDOM/script.js
+++ manipulacionDOM/script.js
@@ -6,9 +6,12 @@ const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
+//Estructura de addEventListener (Agrega un escuchador de eventos) es un método en JavaScript que se utiliza para agregar controladores de eventos a elementos HTML de manera más flexible y modular que la asignación de atributos (onclick, etc).
+//elemento.addEventListener(evento, función [opciones, ]);
+
+btn.addEventListener('click', btnOnClick);
+
 function btnOnClick() {
     const sumaInputs = input1.value + input2.value;
     pResult.innerText = "Resultado: " + sumaInputs;
 };
-//Se genera constante sumaInputs dentro de funcion para guardar los valores de entrada de los inputs.
-//pResult.innertext - pResult hace referencia al parrafo del HTML, .innerText para insertar texto en el HTML desde JS = cadena de texto mas la contante con el valor de la suma de inputs.
