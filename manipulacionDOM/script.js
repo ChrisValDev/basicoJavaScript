6b450c6b116004b77d755ad29b94f4a9aae99b32
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-02 10:26:05 -0600

    Mostrando resultado en la misma pagina con JS y HTML

--- manipulacionDOM/script.js
+++ manipulacionDOM/script.js
@@ -4,8 +4,11 @@ const h1 = document.querySelector('h1');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
 
 function btnOnClick() {
-    console.log(input1.value + input2.value);
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
 };
-//En esta funcion se estan concatenando los valores de los dos inputs ya que guardan la informacion como cadena de texto.
+//Se genera constante sumaInputs dentro de funcion para guardar los valores de entrada de los inputs.
+//pResult.innertext - pResult hace referencia al parrafo del HTML, .innerText para insertar texto en el HTML desde JS = cadena de texto mas la contante con el valor de la suma de inputs.
