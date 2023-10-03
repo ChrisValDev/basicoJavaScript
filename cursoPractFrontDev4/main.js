9fb6ec99c1459df5fe4bba5b4db47b6b04d5a92f
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-03 16:18:19 -0600

    Creando elementos existentes en clase6.html desde JS

--- cursoPractFrontDev4/main.js
+++ cursoPractFrontDev4/main.js
@@ -48,3 +48,60 @@ function toggleShoppingCar() {
     }
     shoppingCar.classList.toggle('inactive');
 }
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: './icons/bt_add_to_cart.svg'
+});
+productList.push({
+    name: "Pantalla",
+    price: 220,
+    image: './icons/bt_add_to_cart.svg'
+});
+productList.push({
+    name: "Computadora",
+    price: 620,
+    image: './icons/bt_add_to_cart.svg'
+});
+
+/*
+<div class="product-card">
+    <img src="https://images.pexels.com/photos/819805/pexels-photo-819805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+    <div class="product-info">
+        <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+        </div>
+    
+        <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>
+</div>
+*/
+
+for (product of productList) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    const productInfoFigure = document.createElement('figure');
+
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+}
