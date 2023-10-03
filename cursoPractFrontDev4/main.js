33b808f7bc55c6005b30f2d78d21d5f3d1a46f9c
ChrisValDev <jimenez.valverde0805@gmail.com>
2023-10-03 12:04:44 -0600

    Condicional dentro de funcion, si se abre carrito de compras, se cierra mobile-menu

--- cursoPractFrontDev4/main.js
+++ cursoPractFrontDev4/main.js
@@ -7,6 +7,8 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
 }
+
+
 // Menu de mobile
 const menuImg = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
@@ -16,12 +18,20 @@ menuImg.addEventListener('click', toggleMobileMenu);
 function toggleMobileMenu() {
     mobileMenu.classList.toggle("inactive");
 }
-// Menu shopping car
+
+
+// Menu shoppingCar
 const viewCarrito = document.querySelector('.navbar-shopping-cart');
 const shoppingCarMenu = document.querySelector('.product-detail');
 
-viewCarrito.addEventListener('click', detalleByBuy);
+viewCarrito.addEventListener('click', toggleShoppingCar);
 
-function detalleByBuy() {
+function toggleShoppingCar() {
+    const mobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+    if (!mobileMenuClosed) {
+        mobileMenu.classList.add("inactive");
+    }
     shoppingCarMenu.classList.toggle('inactive');
 }
+// Generando una condicional dentro de funcion para que cuando se abra el carrito de compras, se cierre el menu mobile en caso deque se encuentre abierto.
